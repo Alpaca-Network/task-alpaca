2cfb6f4f94a13d4b93e3522a6f4591c92cca26f2
Aladeenb <alabtk@gmail.com>
2024-07-06 16:00:02 +0200

    Formatted contract

--- on-chain/contracts/Registry.sol
+++ on-chain/contracts/Registry.sol
@@ -14,14 +14,14 @@ contract TaskRegistry {
     event TaskCreated(uint256 taskId, string description, uint256 reward, address agent, address evaluator);
     
     function createTask(string memory _description, uint256 _reward, address _agent, address _evaluator) public {
-        Task memory newTask = Task({
-            description: _description,
-            agent: _agent,
-            evaluator: _evaluator,
-            reward: _reward
-        });
-        tasks.push(newTask);
-        emit TaskCreated(tasks.length - 1, _description, _reward, _agent, _evaluator);
+      Task memory newTask = Task({
+          description: _description,
+          agent: _agent,
+          evaluator: _evaluator,
+          reward: _reward
+      });
+      tasks.push(newTask);
+      emit TaskCreated(tasks.length - 1, _description, _reward, _agent, _evaluator);
     }
 
     function getTask(uint256 index) public view returns (string memory, address, address, uint256) {
