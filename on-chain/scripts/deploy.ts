6faf47238ccc5f89c73557db6943cc107ddc5ff1
Romain <rdespoelberch@yahoo.com>
2024-07-06 14:07:04 +0200

    deployment script

--- on-chain/scripts/deploy.ts
+++ on-chain/scripts/deploy.ts
@@ -1,19 +1,18 @@
 import {ethers} from "hardhat";
 
-
 async function main() {
   if (!process.env.ORACLE_ADDRESS) {
     throw new Error("ORACLE_ADDRESS env variable is not set.");
   }
   const oracleAddress: string = process.env.ORACLE_ADDRESS;
-  const incentiveAmount: number = 0.01;
+  const incentiveAmount: string = "0.01";
 
   await deployTask("this is a task description", 100, 1000, incentiveAmount);
 }
 
-async function deployTask(description: string, pricePerExecution: number, IncentiveDurationInBlocks: number, incentiveAmount: number) {
+async function deployTask(description: string, pricePerExecution: number, IncentiveDurationInBlocks: number, incentiveAmount: string) {
     const task = await ethers.deployContract("Task", [description, pricePerExecution, IncentiveDurationInBlocks], {
-        value: ethers.utils.parseEther(incentiveAmount) // Convert Ether to wei
+        value: ethers.parseEther(incentiveAmount) // Convert Ether to wei
     });
   
     await task.waitForDeployment();
