438302a449b2dba2dbfc9289a350cf071456b890
Andrew L <45078400+aliutiev@users.noreply.github.com>
2024-07-06 15:38:26 +0200

    Update Registry.sol

--- on-chain/contracts/Registry.sol
+++ on-chain/contracts/Registry.sol
@@ -8,26 +8,27 @@ pragma solidity ^0.8.13;
 
 import './Task.sol';
 contract Registry {
-  Task[] public registry;
+    Task[] public tasks;
 
-  function createTask(
-    string memory _description,
-    uint _pricePerExecution,
-    uint256 _incentiveBlocksDuration
-  ) public payable {
-    Task task = new Task(
-      _description,
-      _pricePerExecution,
-      _incentiveBlocksDuration
-    );
-    registry.push(task);
-  }
+    event TaskCreated(uint256 taskId, string description, uint256 reward, address agent, address evaluator);
+    
+    function createTask(string memory _description, uint256 _reward, address _agent, address _evaluator) public {
+        Task memory newTask = Task({
+            description: _description,
+            agent: _agent,
+            evaluator: _evaluator,
+            reward: _reward
+        });
+        tasks.push(newTask);
+        emit TaskCreated(tasks.length - 1, _description, _reward, _agent, _evaluator);
+    }
 
-  function getTask(uint index) public view returns (Task) {
-    return registry[index];
-  }
+    function getTask(uint256 index) public view returns (Task memory) {
+        require(index < tasks.length, "Task does not exist");
+        return tasks[index];
+    }
 
-  function getRegistry() public view returns (Task[] memory) {
-    return registry;
-  }
+    function getRegistry() public view returns (Task[] memory) {
+        return tasks;
+    }
 }
